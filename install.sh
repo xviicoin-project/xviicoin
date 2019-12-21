fa42e3151d2dd6c4265a78c1e4189280abf0fd35
xviicoin-project <48865757+xviicoin-project@users.noreply.github.com>
2019-12-21 20:22:17 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,8 +1,16 @@
 #!/bin/bash
 
-ECHO "Installing Dependencies"
+echo "  "
+echo "  "
+echo "Installing General Dependencies"
+echo "  "
+echo "  "
 
-sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils libboost-all-dev ibminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
+sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
+
+sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
+
+sudo apt-get install ibminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
 
 sudo apt-get install software-properties-common
 sudo add-apt-repository ppa:bitcoin/bitcoin
@@ -11,5 +19,7 @@ sudo apt-get install libdb4.8-dev libdb4.8++-dev
 
 ./autogen.sh
 ./configure
-
-ECHO "Type MAKE and press ENTER to begin the build process."
+echo "  "
+echo "  "
+echo "Type MAKE and press ENTER to begin the build process."
+echo "  "
