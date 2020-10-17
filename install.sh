c789c7adeb49727fced54c9982250263d2825e1c
xviicoin-project <48865757+xviicoin-project@users.noreply.github.com>
2020-10-17 12:23:35 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,19 +6,18 @@ echo "Installing General Dependencies"
 echo "  "
 echo "  "
 
+sudo apt-get install autoconf
+
 sudo apt-get install build-essential libtool autotools-dev automake pkg-config libssl-dev libevent-dev bsdmainutils
 
 sudo apt-get install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
 
 sudo apt-get install ibminiupnpc-dev libzmq3-dev libqt5gui5 libqt5core5a libqt5dbus5 qttools5-dev qttools5-dev-tools libprotobuf-dev protobuf-compiler libqrencode-dev
 
-sudo apt-get install software-properties-common
-sudo add-apt-repository ppa:bitcoin/bitcoin
-sudo apt-get update
-sudo apt-get install libdb4.8-dev libdb4.8++-dev
+sudo apt-get install libdb++-dev
 
 ./autogen.sh
-./configure
+./configure --with-incompatible-bdb
 echo "  "
 echo "  "
 echo "Type MAKE and press ENTER to begin the build process."
